51bb34db293e328b118483f85290acdf63a43d16
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2023-09-02 12:48:50 +0700

    belajar javascript

---
+++ belajar javascript/javascript.js
@@ -0,0 +1,100 @@
+/*let a=2;
+let b=3;
+let c=a+b;
+console.log(c);*/
+
+
+/*let a=2
+let b=3
+let c=4
+let d=5
+let e=((b+a)*c)/(a*b*b)+(a-d);
+/*3+2*4/2*3*3+2-5*/
+
+
+
+
+/*let x=6;
+let y=5;
+let hasil=x>y;
+
+if(x>y){
+    console.log("benar")
+}else{
+    console.log("salah")
+}*/
+
+let kkm=80;
+let nilai=40;
+
+if (nilai<kkm){
+    condole.log("Kamu lulus");
+}else{
+    console.log("tidak lulus");
+}
+
+
+
+let bulan=1;
+let tanggal=35;
+let zodiak="Belum dibuat";
+
+if(bulan==1){
+    if(tanggal>0 && tanggal<20){
+        zodiak="aquarius";
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="Virgo";
+    }
+} 
+
+if(bulan==1){
+    if(tanggal>0 && tanggal<20){
+        zodiak="taurus"
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="capicorn"
+    }
+}
+if(bulan==2){
+    if(tanggal>0 && tanggal<20){
+        zodiak="aries"
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="gemini"
+    }
+}
+if(bulan==3){
+    if(tanggal>0 && tanggal<20){
+        zodiak="cancer"
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="leo"
+    }
+}
+if(bulan==4){
+    if(tanggal>0 && tanggal<20){
+        zodiak="virgo"
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="libra"
+    }
+}
+if(bulan==5){
+    if(tanggal>0 && tanggal<20){
+        zodiak="scorpio"
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="sagitarius"
+    }
+}
+if(bulan==6){
+    if(tanggal>0 && tanggal<20){
+        zodiak="aquarius"
+    }
+    if(tanggal>19 && tanggal<32){
+        zodiak="pisces"
+    }
+}
+
+    console.log(zodiak);
